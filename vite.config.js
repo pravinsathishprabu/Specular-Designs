67780310ef5a80558f368e8b65dfd846a7beeb94
pravinsathishprabu <pravin592002@gmail.com>
2025-05-04 07:55:14 +0530

    new commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure the output directory is 'dist'
+    outDir: 'build', // Ensure the output directory is 'dist'
   },
 });
