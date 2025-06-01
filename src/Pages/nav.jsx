7868bb896661f50d3b4512cfa26607cf2123f7f2
pravinsathishprabu <pravin592002@gmail.com>
2025-06-01 06:22:12 +0530

    changes-01-02

--- src/Pages/nav.jsx
+++ src/Pages/nav.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/SPECULAR_LOGO.png';
 
+
 const Navbar = () => {
   // ✅ Collapse handler
   const handleNavLinkClick = () => {
